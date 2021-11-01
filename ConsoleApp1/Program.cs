02c60843c896fa80a8895abdd64c365bf074ad61
halilkorukmez <halilkorukmez@clebracode.com>
2021-11-01 05:06:48 +0300

    new

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı girin: ");
+            string surName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + "  " + surName);
+        }
+    }
+}
